f4cd84625dbb83ca6e8fecb5e12c67bb381ab75a
Binaya Raj Shrestha <bineerash@gmail.com>
2013-04-30 10:40:46 +0545

    Committed new files.

--- js/main.js
+++ js/main.js
@@ -16,11 +16,16 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 var app = {
+
+
+
     // Application Constructor
     initialize: function() {
         $.support.cors = true;
         this.bindEvents();
+        $("#camera").hide();
     },
     // Bind Event Listeners
     //
@@ -36,6 +41,7 @@ var app = {
         $( "div.box" ).on( 'swipeleft', this.swipeHandlerLeft );
         $( "div.box" ).on( 'swiperight', this.swipeHandlerRight );
         $( "div.box" ).on( 'taphold', this.tapHoldHandler );
+//        $("div#back").on('swipeleft', this.goBack);
     },
     // deviceready Event Handler
     //
@@ -43,6 +49,8 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         alert("device Ready");
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
@@ -67,15 +75,121 @@ var app = {
     swipeHandlerLeft:function(event){
         $(event.target).removeClass("swipeRight");
         $(event.target).addClass("swipeLeft");
+//        $.mobile.changePage("index.html");
+        $("#camera").hide();
+        $("#main").show();
+
     },
     swipeHandlerRight:function(event){
         $(event.target).removeClass("swipeLeft");
         $(event.target).addClass("swipeRight");
+        $("#main").hide();
+        $("#camera").show();
+        //$.mobile.changePage("#camera");
+        //alert("page changed");
     },
     tapHoldHandler:function(event){
         $(event.target).removeClass("swipeLeft swipeRight");
         $(event.target).addClass("box");
-    }
+    },
+    goBack:function(){
+        $.mobile.changePage("#main");
+    },
+
+    // Called when a photo is successfully retrieved
+    //
+     onPhotoDataSuccess: function(imageData) {
+    // Uncomment to view the base64 encoded image data
+    // console.log(imageData);
+
+    // Get image handle
+    //
+    var smallImage = document.getElementById('smallImage');
+
+    // Unhide image elements
+    //
+    smallImage.style.display = 'block';
+
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    smallImage.src = "data:image/jpeg;base64," + imageData;
+},
+
+// Called when a photo is successfully retrieved
+    //
+     onPhotoDataSuccess: function(imageData) {
+    // Uncomment to view the base64 encoded image data
+    // console.log(imageData);
+
+    // Get image handle
+    //
+    var smallImage = document.getElementById('smallImage');
+
+    // Unhide image elements
+    //
+    smallImage.style.display = 'block';
+
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    smallImage.src = "data:image/jpeg;base64," + imageData;
+},
+
+// Called when a photo is successfully retrieved
+//
+ onPhotoURISuccess: function(imageURI) {
+    // Uncomment to view the image file URI
+    // console.log(imageURI);
+
+    // Get image handle
+    //
+    var largeImage = document.getElementById('largeImage');
+
+    // Unhide image elements
+    //
+    largeImage.style.display = 'block';
+
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    largeImage.src = imageURI;
+},
+
+// A button will call this function
+//
+ capturePhoto: function() {
+    // Take picture using device camera and retrieve image as base64-encoded string
+    navigator.camera.getPicture(this.onPhotoDataSuccess, this.onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+},
+
+// A button will call this function
+//
+ capturePhotoEdit: function() {
+    // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+    navigator.camera.getPicture(this.onPhotoDataSuccess, this.onFail, { quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL });
+},
+
+// A button will call this function
+//
+ getPhoto: function(source) {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(this.onPhotoURISuccess, this.onFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+},
+
+// Called if something bad happens.
+//
+ onFail: function(message) {
+    alert('Failed because: ' + message);
+}
+
+
 
 
 };
+var pictureSource;
+var destinationType;
